b887291fb29cb010b72fbbbc8d63090e9d8ff146
Arafath <alarafath.info@gmail.com>
2026-07-15 03:47:14 +0600

    check jwt authentication

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-	"ecommerce/cmd"
+	"ecommerce/util"
+	"fmt"
 )
 
 func main() {
-	cmd.Serve()
+	// cmd.Serve()
+	jwt, err := util.CreateJwt("my-secret", util.Payload{
+		Sub:         45,
+		FirstName:   "Arafath",
+		LastName:    "Rahman",
+		Email:       "arafath@gmail.com",
+		IsShopOwner: false,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(jwt)
 }
